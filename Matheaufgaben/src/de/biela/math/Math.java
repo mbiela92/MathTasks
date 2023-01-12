780bbd135ab32f996f1742633c5fe49aaec6a40e
mbiela92 <113643394+mbiela92@users.noreply.github.com>
2023-01-12 16:47:57 +0100

    Inital commit

---
+++ Matheaufgaben/src/de/biela/math/Math.java
@@ -0,0 +1,223 @@
+package de.biela.math;
+
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.PageSize;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+
+
+
+
+public class Math
+{
+    /*
+     * Setter und Getter habe ich weggelassen, weil es soll ja nichts mehr verändert werden. Dann verwirrt es auch niemanden
+     * 
+     * 
+     */
+    private static String []arrayOperator;
+    private int firstOperand;
+    private int secondOperand;
+    private static int MAX_VALUE=20;
+    private int result=0;
+    private static int amountTasks=50;
+    private String operator;
+    private static String path="C:/Users/marce/OneDrive/Desktop/result.pdf";
+    private Random random = new Random ( ) ;
+    
+    protected Math() 
+    {
+        firstOperand = random.nextInt(MAX_VALUE)+1;                       //Zahl zwischen 1-20
+        operator = arrayOperator[random.nextInt(arrayOperator.length)];
+        secondOperand = determineSecondOperand(MAX_VALUE);
+    }
+    
+    protected int determineSecondOperand(int value)
+    {
+        int zweiterOperand = 999;
+        switch(operator)
+        {
+            case"+":
+                zweiterOperand = random.nextInt(value+1-firstOperand);      
+                break;
+            case"-":
+                zweiterOperand = random.nextInt(firstOperand);       
+                break;
+            case"*":
+                zweiterOperand = random.nextInt((value/firstOperand)+1);   
+                break;
+            case":":
+                while(result==0||result==1) 
+                {
+                    zweiterOperand = random.nextInt(firstOperand)+1;
+                    result =  firstOperand/zweiterOperand;
+                }
+                firstOperand = result*zweiterOperand;
+                result=0;
+                break;
+            }
+        return zweiterOperand;
+    }
+    
+    private static String formatTasks(List liste) 
+    {
+        StringBuilder sb= new StringBuilder();
+        while(!liste.isEmpty()) 
+        {
+            for(int i=0;i<3;i++) 
+            {
+                if(!liste.isEmpty())sb.append(String.format("%-30s",liste.remove(0).toString()));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+    protected static String[] FormatArithmeticOperations(String input) {
+        String [] tmp = input.split("");
+        return tmp;
+    }
+    
+    protected static int queryAmount() 
+    {
+        int tmp = 999;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte tipp eine Zahl zwischen 1-50 ein um zu waehlen wie viele Aufgaben erstellt werden sollen");
+        while (scanner.hasNext()) 
+        {
+            if (scanner.hasNextInt()) 
+            {
+               int input = scanner.nextInt();
+               if(input>0&&input<=50)
+               {
+                   System.out.println(input + " Aufgaben werden erstellt");
+                   tmp=input;
+                   break;
+               }
+               else{
+                   System.out.println("Bedingungen wurden nicht erfuellt");
+               }
+            }else{
+                System.out.println(scanner.next() + " das ist keine reine Zahl");
+            }
+        }
+        return tmp;
+    }
+ 
+    
+    protected static String queryArithmeticOperand() //Mache ich die Methode protected oder package?
+    {
+        String tmp="999";
+        String input="";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte waehle bis zu 4 Rechenoperationen aus + - * :\nNur die Rechenzeichen eingeben ohne Leerstelle");
+        while(scanner.hasNext()) 
+        {
+            if(scanner.hasNext()) {
+                input = scanner.next();
+            }
+            if(input.matches("[\\+\\-\\*:]{0,4}")) {
+                tmp=input;
+                break;
+            }
+            else {System.out.println("Das passt nicht nur die Rechenzeicheneingeben");} 
+        }
+        System.out.println("Sie haben folgende Rechenzeichen gewaehlt " + tmp + "\nund das Dokument wurde erstellt");
+        scanner.close();
+        return tmp;
+    }
+    
+    protected static List<Math> createTasks(int amount) 
+    {
+        List<Math> tasks = new ArrayList<Math>();
+        for(int i = 0; i<amount;i++) 
+        {
+            tasks.add(new Math());
+        }
+        return tasks;
+    }
+    
+    private static void createDocument(String text, String path) {
+        Document document = new Document(PageSize.A4, 50,50,50,50);
+        try
+        {
+            PdfWriter.getInstance(document, new FileOutputStream(path));
+        }
+        catch (FileNotFoundException e1)
+        {
+            e1.printStackTrace();
+        }
+        catch (DocumentException e1)
+        {
+            e1.printStackTrace();
+        }
+        document.open();
+        try
+        {
+            document.add(new Paragraph(text));
+        }
+        catch (DocumentException e)
+        {
+            e.printStackTrace();
+        }
+        document.close();
+    }
+    
+    protected static int queryMaxValue()
+    {
+        int tmp=999;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte tippe eine Zahl zwischen 1-100 ein um zu waehlen wie hoch die Rechenoperationen sein sollen");
+        while (scanner.hasNext()) 
+        {
+            if (scanner.hasNextInt()) 
+            {
+               int input = scanner.nextInt();
+               if(input>0&&input<=100)
+               {
+                   tmp=input;
+                   System.out.println("Aufgaben in Hoehe von maximal " + tmp + " werden erstellt");
+                   break;
+               }
+               else{
+                   System.out.println("Bedingungen wurden nicht erfuellt");
+               }
+            }else{
+                System.out.println(scanner.next() + " das ist keine reine Zahl");
+            }
+            
+        }
+        return tmp;
+    }
+    
+
+    public String toString() 
+    {
+        return firstOperand + " " + operator + " " + secondOperand + " = ____";
+    }
+    
+
+    public static void main(String[] args)
+    {
+        amountTasks = queryAmount();
+        MAX_VALUE = queryMaxValue();
+        arrayOperator = Math.FormatArithmeticOperations(queryArithmeticOperand());
+        String text = Math.formatTasks(createTasks(amountTasks));
+        Math.createDocument(text, path);
+
+    }
+
+    
+
+    
+
+}
---
+++ Matheaufgaben/src/de/biela/math/MathTests.java
@@ -0,0 +1,190 @@
+package de.biela.math;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class MathTests
+{
+    final int FAILURE=999;
+    private void systemInput (String userInput) {
+        String input = userInput;
+        InputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+    }
+    
+    @ParameterizedTest(name = "run {index}: QueryAmount Input {0} Output {1}")
+    @CsvSource(delimiter = '|', textBlock = """
+        -1      |       999
+        0       |       999   
+        1       |       1                 
+        49      |       49
+        50      |       50
+        51      |       999
+        k10     |       999
+        1k0     |       999
+        10k     |       999
+
+        
+        
+    """)
+    
+    void TestQueryAmount(String first, int second)
+    {
+        assertInputAmount(first, second);
+    }
+    
+    private void assertInputAmount (String input,final int output) 
+    {
+        // GIVEN
+        systemInput(input);
+        
+        // WHEN
+        final int result = Math.queryAmount();
+        
+        // THEN
+        final int expected = output;
+        assertEquals(expected, result);
+    }
+    
+    @ParameterizedTest(name = "run {index}: TestQueryMaxValue Input {0} Output {1}")
+    @CsvSource(delimiter = '|', textBlock = """
+        -1      |       999
+        0       |       999   
+        1       |       1                 
+        99      |       99
+        100     |       100
+        101     |       999
+        k10     |       999
+        1k0     |       999
+        10k     |       999
+
+        
+        
+    """)
+    
+    void TestQueryMaxValue(String first, int second)
+    {
+        assertInputMAXVALUE(first, second);
+    }
+    
+    private void assertInputMAXVALUE (String input,final int output) 
+    {
+            // GIVEN
+            systemInput(input);
+            
+            // WHEN
+            final int result = Math.queryMaxValue();
+        
+            // THEN
+            final int expected = output;
+            assertEquals(expected, result);
+    }
+    
+    @ParameterizedTest(name = "run {index}: TestQueryArithmeticOperand Input {0} Output {1}")
+    @CsvSource(delimiter = '|', textBlock = """
+        +       |       +
+        -       |       -   
+        *       |       *                 
+        :       |       :
+        +-*:    |       +-*:
+        ++++    |       ++++
+        +++++   |       999
+        100     |       999
+        101     |       999
+        k10     |       999
+        1k0     |       999
+        10k     |       999
+
+        
+        
+    """)
+    
+    void TestQueryArithmeticOperand(String first, String second)
+    {
+        assertInputArithmeticOperand(first, second);
+    }
+    
+    private void assertInputArithmeticOperand (String input,final String output) 
+    {
+            // GIVEN
+            systemInput(input);
+            
+            // WHEN
+            final String result = Math.queryArithmeticOperand();
+            
+            // THEN
+            final String expected = output;
+            assertEquals(expected, result);
+    }
+    
+    @ParameterizedTest(name = "run {index}: TestFormatArithmeticOperand Input {0} Output {1}")
+    @MethodSource("InputStringAndExpectedArray")
+    
+    void TestFormatArithmeticOperand(String first, String[] second)
+    {
+        assertInputFormatArithmeticOperand(first, second);
+    }
+    
+    private static void assertInputFormatArithmeticOperand (String input,final String[] output) 
+    {
+            // GIVEN
+            final String[] result;
+            
+            // WHEN
+            result = Math.FormatArithmeticOperations(input);
+            
+            // THEN
+            final String[] expected = output;
+            assertArrayEquals(expected, result);
+    }
+    
+    static Stream<Arguments> InputStringAndExpectedArray ()
+    {
+        String value1 = "+-*:";
+        String[] expected1 = {"+","-","*",":"};
+        
+        String value2 = "+-*";
+        String[] expected2 = {"+","-","*"};
+        
+        String value3 = "+-";
+        String[] expected3 = {"+","-"};
+        
+        String value4 = "+";
+        String[] expected4 = {"+"};       
+
+        return Stream.of(Arguments.of(value1,expected1),
+                         Arguments.of(value2,expected2),
+                         Arguments.of(value3,expected3),
+                         Arguments.of(value4,expected4)); 
+    }
+    
+    
+    /*
+    @ParameterizedTest(name = "run {index}: TestCreateTasks Input {0}")
+    @CsvSource({"5","50","100"})
+    
+    void TestCreateTasks(int first)
+    {
+        // GIVEN
+        int length = first;
+        // WHEN
+        int expected = Math.createTasks(first).size();
+        // THEN
+        
+        assertTrue(length==expected);
+        
+        
+    }
+    */
+
+    
+}
