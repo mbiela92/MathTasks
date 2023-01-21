8e0c91bb51ebb8db211647d5774635fefe54e43d
mbiela92 <development@marcel-biela.de>
2023-01-21 16:37:57 +0100

    MoreUnit integriert

--- Matheaufgaben/src/de/biela/math/MathTest.java
+++ Matheaufgaben/src/de/biela/math/MathTest.java
@@ -13,7 +13,7 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
-class MathTests
+class MathTest
 {
     final int FAILURE=999;
     private void systemInput (String userInput) {
